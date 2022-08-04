0d1c11c9fd816d8939ae442d6a3c75c67340f809
ivana <i.alvarez@rsegrp.com>
2022-08-04 16:55:32 -0400

    ajustes en monitorizacion

--- src/components/MonitoringForm/Nodes/ReadNodes.tsx
+++ src/components/MonitoringForm/Nodes/ReadNodes.tsx
@@ -1,136 +1,83 @@
-import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import Chip from 'ui-component/extended/Chip'
-
-// import VisibilityIcon from '@material-ui/icons/Visibility'
-// import VisibilityIcon from '@material-ui/icons/Visibility'
-// import SelectColumnFilter from 'components/Table/Filters/SelectColumnFilter'
-// import EditIcon from '@material-ui/icons/Edit'
-// import { IconButton } from '@material-ui/core'
-
-import TableCustom from 'components/Table'
 import AnimateButton from 'ui-component/extended/AnimateButton'
 import { Button, Grid } from '@material-ui/core'
+import MainCard from 'ui-component/cards/MainCard'
 
-const columns = [
-    {
-        Header: 'Nombre',
-        accessor: 'name',
-    },
-    {
-        Header: 'Tipo',
-        accessor: 'node_type',
-    },
-    {
-        Header: 'Código',
-        accessor: 'node_code',
-    },
-
-    {
-        Header: 'Estatus',
-        accessor: 'active',
-        disableFilters: true,
-    },
-    {
-        Header: 'Última actualización',
-        accessor: 'updated_on',
-    },
-
-    // {
-    //     Header: 'Acciones',
-    //     accessor: 'edit',
-    //     disableFilters: true,
-    // },
-]
 interface monitoringProps {
     monitoringData?: any
 }
 
 const ReadNodes = ({ monitoringData }: monitoringProps) => {
-    const [rowsInitial, setRowsInitial] = React.useState<Array<any>>([])
     const navigate = useNavigate()
-
-    const handleEdit = React.useCallback(
-        (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-            e.preventDefault()
-            const id = e.currentTarget.dataset.id
-            navigate(`/gestion-de-cuentas-usuarios/editar/${id}`)
-        },
-        [navigate]
-    )
-    // const handleView = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    //     e.preventDefault()
-    //     const id = e.currentTarget.dataset.id
-    //     navigate(`/gestion-de-tarifas/editar/${id}-view`)
-    // }
 
     const handleReturn = () => {
         navigate(-1)
     }
 
-    React.useEffect(() => {
-        const rows = monitoringData.map(
-            ({ name, node_type, node_code, updated_on, active }) => ({
-                name,
-                node_type,
-                node_code,
-                updated_on,
-                active: active ? (
-                    <Chip
-                        label="Activo"
-                        size="small"
-                        chipcolor="success"
-                        sx={{ width: '96px' }}
-                    />
-                ) : (
-                    <Chip
-                        label="Inactivo"
-                        size="small"
-                        chipcolor="orange"
-                        sx={{ width: '96px' }}
-                    />
-                ),
-                // edit: (
-                //     <div className="flex">
-                //         <button data-id={id} onClick={handleEdit}>
-                //             <IconButton color="primary">
-                //                 <VisibilityIcon sx={{ fontSize: '1.3rem' }} />
-                //             </IconButton>
-                //         </button>
-                //     </div>
-                // ),
-            })
-        )
-        setRowsInitial(rows)
-    }, [handleEdit, monitoringData])
+    const prodImage = require.context('../../../../public/', true)
 
     return (
         <>
-            <TableCustom
-                columns={columns}
-                data={rowsInitial}
-                // title="Nodos"
-                // addIconTooltip="Crear usuario"
-                // handleCreate={handleCreate}
-            />
-            <Grid
-                item
-                sx={{
-                    display: 'flex',
-                    justifyContent: 'flex-end',
-                    marginTop: '15px',
-                }}
-            >
-                <AnimateButton>
-                    <Button
-                        variant="contained"
-                        size="medium"
-                        onClick={handleReturn}
-                    >
-                        Volver
-                    </Button>
-                </AnimateButton>
-            </Grid>
+            <MainCard content={true}>
+                <div className=" grid sm:grid-cols-1 md:grid-cols-4 ">
+                    {monitoringData.map(({ image, node_code, active }) => {
+                        const prodProfile =
+                            image && prodImage(`./${image}`).default
+                        return (
+                            <>
+                                <div className="flex flex-col ml-28">
+                                    <img
+                                        src={prodProfile}
+                                        alt="monitorizacion"
+                                        width="70px"
+                                        height="70px"
+                                    />
+                                    <div className="font-bold uppercase mt-1 ">
+                                        <span>{node_code}</span>
+                                    </div>
+                                    <div className="mt-2">
+                                        {active ? (
+                                            <Chip
+                                                label="Activo"
+                                                size="small"
+                                                chipcolor="success"
+                                                sx={{ width: '96px' }}
+                                            />
+                                        ) : (
+                                            <Chip
+                                                label="Inactivo"
+                                                size="small"
+                                                chipcolor="orange"
+                                                sx={{ width: '96px' }}
+                                            />
+                                        )}
+                                    </div>
+                                </div>
+                            </>
+                        )
+                    })}
+                </div>
+
+                <Grid
+                    item
+                    sx={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        marginTop: '15px',
+                    }}
+                >
+                    <AnimateButton>
+                        <Button
+                            variant="contained"
+                            size="medium"
+                            onClick={handleReturn}
+                        >
+                            Volver
+                        </Button>
+                    </AnimateButton>
+                </Grid>
+            </MainCard>
         </>
     )
 }
