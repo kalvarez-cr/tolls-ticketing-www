fb00466bb321a084218d0088dfc521b2ead208fb
Carlos Graterol <carlos.graterol@icloud.com>
2022-08-23 15:16:40 -0400

    Fix dark mode in analytics and dashboard

--- src/components/reportsForTime/TimeAnalysisChart.tsx
+++ src/components/reportsForTime/TimeAnalysisChart.tsx
@@ -10,6 +10,7 @@ import {
 } from 'recharts'
 import { Tab } from '@headlessui/react'
 import { useNavigate } from 'react-router'
+import { useTheme } from '@material-ui/core'
 
 interface TimeAnalysisChartProps {
     data?: any
@@ -17,6 +18,7 @@ interface TimeAnalysisChartProps {
 
 const TimeAnalysisChart = ({ data }: TimeAnalysisChartProps) => {
     const navigate = useNavigate()
+    const theme = useTheme()
     const summaries: any[] = []
 
     for (const item of data.data) {
@@ -32,106 +34,211 @@ const TimeAnalysisChart = ({ data }: TimeAnalysisChartProps) => {
     }
 
     return (
-        <div className="p-4 bg-white rounded-xl">
-            <Tab.Group>
-                <div className="flex flex-col w-full mb-8 justify-center items-center gap-6 lg:flex-row lg:justify-between">
-                    <Tab.List className="flex p-2 space-x-2 order-last rounded-xl bg-green-700 bg-opacity-60 lg:order-first">
-                        <Tab
-                            className={({ selected }) =>
-                                classNames(
-                                    'w-full rounded-lg p-4 text-sm font-bold text-white',
-                                    'ring-white ring-opacity-60 ring-offset-2 ring-offset-green-100 focus:outline-none focus:ring-2',
-                                    selected
-                                        ? 'bg-green-200 bg-opacity-40 shadow-md'
-                                        : 'text-white hover:bg-opacity-50 hover:text-white'
-                                )
-                            }
-                        >
-                            Ingresos
-                        </Tab>
-                        <Tab
-                            className={({ selected }) =>
-                                classNames(
-                                    'w-full rounded-lg p-4 text-sm font-bold text-white',
-                                    'ring-white ring-opacity-60 ring-offset-2 ring-offset-green-100 focus:outline-none focus:ring-2',
-                                    selected
-                                        ? 'bg-green-200 bg-opacity-40 shadow-md'
-                                        : 'text-white hover:bg-green-200 hover:bg-opacity-30 hover:text-white'
-                                )
-                            }
-                        >
-                            Vehículos
-                        </Tab>
-                    </Tab.List>
-                    <h2 className="font-bold text-2xl pb-1 mt-0 mr-0 sm:mt-3 ">
-                        Análisis de flujo
-                    </h2>
-                    <button
-                        type="button"
-                        className="lg:mr-1 bg-green-700 bg-opacity-70 hover:bg-opacity-50 rounded-lg shadow-lg p-4 font-bold text-white"
-                        onClick={handleReturn}
-                    >
-                        Nuevo reporte
-                    </button>
+        <>
+            {theme.palette.mode === 'dark' ? (
+                <div className="p-4 bg-gray-900 bg-opacity-20 rounded-xl">
+                    <Tab.Group>
+                        <div className="flex flex-col w-full mb-8 justify-center items-center gap-6 lg:flex-row lg:justify-between">
+                            <Tab.List className="flex p-2 space-x-2 order-last rounded-xl bg-green-900 bg-opacity-40 lg:order-first">
+                                <Tab
+                                    className={({ selected }) =>
+                                        classNames(
+                                            'w-full rounded-lg p-4 text-sm font-bold text-white',
+                                            'ring-white ring-opacity-60 ring-offset-2 ring-offset-green-100 focus:outline-none focus:ring-2',
+                                            selected
+                                                ? 'bg-green-700 bg-opacity-30 shadow-md'
+                                                : 'text-white hover:bg-opacity-40 hover:text-white'
+                                        )
+                                    }
+                                >
+                                    Ingresos
+                                </Tab>
+                                <Tab
+                                    className={({ selected }) =>
+                                        classNames(
+                                            'w-full rounded-lg p-4 text-sm font-bold text-white',
+                                            'ring-white ring-opacity-60 ring-offset-2 ring-offset-green-100 focus:outline-none focus:ring-2',
+                                            selected
+                                                ? 'bg-green-700 bg-opacity-30 shadow-md'
+                                                : 'text-white hover:bg-green-200 hover:bg-opacity-40 hover:text-white'
+                                        )
+                                    }
+                                >
+                                    Vehículos
+                                </Tab>
+                            </Tab.List>
+                            <h2 className="text-white font-bold text-2xl pb-1 mt-0 mr-0 sm:mt-3 ">
+                                Análisis de flujo
+                            </h2>
+                            <button
+                                type="button"
+                                className="lg:mr-1 bg-green-900 bg-opacity-70 hover:bg-opacity-50 rounded-lg shadow-lg p-4 font-bold text-white"
+                                onClick={handleReturn}
+                            >
+                                Nuevo reporte
+                            </button>
+                        </div>
+                        <Tab.Panels>
+                            <Tab.Panel>
+                                <ResponsiveContainer width="100%" height={730}>
+                                    <BarChart
+                                        width={1100}
+                                        height={600}
+                                        data={summaries}
+                                        margin={{
+                                            top: 5,
+                                            right: 20,
+                                            left: 0,
+                                            bottom: 5,
+                                        }}
+                                    >
+                                        <CartesianGrid strokeDasharray="3 3" />
+                                        <XAxis dataKey="date" />
+                                        <YAxis />
+                                        <Tooltip />
+                                        <Legend />
+                                        <Bar
+                                            type="monotone"
+                                            dataKey="subtotal_value"
+                                            name="Ingresos"
+                                            fill="#60A5FA"
+                                        />
+                                    </BarChart>
+                                </ResponsiveContainer>
+                            </Tab.Panel>
+                            <Tab.Panel>
+                                <ResponsiveContainer width="100%" height={730}>
+                                    <BarChart
+                                        width={1100}
+                                        height={600}
+                                        data={summaries}
+                                        margin={{
+                                            top: 5,
+                                            right: 20,
+                                            left: 0,
+                                            bottom: 5,
+                                        }}
+                                    >
+                                        <CartesianGrid strokeDasharray="3 3" />
+                                        <XAxis dataKey="date" />
+                                        <YAxis />
+                                        <Tooltip />
+                                        <Legend />
+                                        <Bar
+                                            type="monotone"
+                                            dataKey="vehicles"
+                                            name="Vehículos"
+                                            fill="#60A5FA"
+                                        />
+                                    </BarChart>
+                                </ResponsiveContainer>
+                            </Tab.Panel>
+                        </Tab.Panels>
+                    </Tab.Group>
                 </div>
-                <Tab.Panels>
-                    <Tab.Panel>
-                        <ResponsiveContainer width="100%" height={730}>
-                            <BarChart
-                                width={1100}
-                                height={600}
-                                data={summaries}
-                                margin={{
-                                    top: 5,
-                                    right: 20,
-                                    left: 0,
-                                    bottom: 5,
-                                }}
+            ) : (
+                <div className="p-4 bg-white rounded-xl">
+                    <Tab.Group>
+                        <div className="flex flex-col w-full mb-8 justify-center items-center gap-6 lg:flex-row lg:justify-between">
+                            <Tab.List className="flex p-2 space-x-2 order-last rounded-xl bg-green-700 bg-opacity-60 lg:order-first">
+                                <Tab
+                                    className={({ selected }) =>
+                                        classNames(
+                                            'w-full rounded-lg p-4 text-sm font-bold text-white',
+                                            'ring-white ring-opacity-60 ring-offset-2 ring-offset-green-100 focus:outline-none focus:ring-2',
+                                            selected
+                                                ? 'bg-green-200 bg-opacity-40 shadow-md'
+                                                : 'text-white hover:bg-opacity-50 hover:text-white'
+                                        )
+                                    }
+                                >
+                                    Ingresos
+                                </Tab>
+                                <Tab
+                                    className={({ selected }) =>
+                                        classNames(
+                                            'w-full rounded-lg p-4 text-sm font-bold text-white',
+                                            'ring-white ring-opacity-60 ring-offset-2 ring-offset-green-100 focus:outline-none focus:ring-2',
+                                            selected
+                                                ? 'bg-green-200 bg-opacity-40 shadow-md'
+                                                : 'text-white hover:bg-green-200 hover:bg-opacity-30 hover:text-white'
+                                        )
+                                    }
+                                >
+                                    Vehículos
+                                </Tab>
+                            </Tab.List>
+                            <h2 className="font-bold text-2xl pb-1 mt-0 mr-0 sm:mt-3 ">
+                                Análisis de flujo
+                            </h2>
+                            <button
+                                type="button"
+                                className="lg:mr-1 bg-green-700 bg-opacity-70 hover:bg-opacity-50 rounded-lg shadow-lg p-4 font-bold text-white"
+                                onClick={handleReturn}
                             >
-                                <CartesianGrid strokeDasharray="3 3" />
-                                <XAxis dataKey="date" />
-                                <YAxis />
-                                <Tooltip />
-                                <Legend />
-                                <Bar
-                                    type="monotone"
-                                    dataKey="subtotal_value"
-                                    name="Ingresos"
-                                    fill="#60A5FA"
-                                />
-                            </BarChart>
-                        </ResponsiveContainer>
-                    </Tab.Panel>
-                    <Tab.Panel>
-                        <ResponsiveContainer width="100%" height={730}>
-                            <BarChart
-                                width={1100}
-                                height={600}
-                                data={summaries}
-                                margin={{
-                                    top: 5,
-                                    right: 20,
-                                    left: 0,
-                                    bottom: 5,
-                                }}
-                            >
-                                <CartesianGrid strokeDasharray="3 3" />
-                                <XAxis dataKey="date" />
-                                <YAxis />
-                                <Tooltip />
-                                <Legend />
-                                <Bar
-                                    type="monotone"
-                                    dataKey="vehicles"
-                                    name="Vehículos"
-                                    fill="#60A5FA"
-                                />
-                            </BarChart>
-                        </ResponsiveContainer>
-                    </Tab.Panel>
-                </Tab.Panels>
-            </Tab.Group>
-        </div>
+                                Nuevo reporte
+                            </button>
+                        </div>
+                        <Tab.Panels>
+                            <Tab.Panel>
+                                <ResponsiveContainer width="100%" height={730}>
+                                    <BarChart
+                                        width={1100}
+                                        height={600}
+                                        data={summaries}
+                                        margin={{
+                                            top: 5,
+                                            right: 20,
+                                            left: 0,
+                                            bottom: 5,
+                                        }}
+                                    >
+                                        <CartesianGrid strokeDasharray="3 3" />
+                                        <XAxis dataKey="date" />
+                                        <YAxis />
+                                        <Tooltip />
+                                        <Legend />
+                                        <Bar
+                                            type="monotone"
+                                            dataKey="subtotal_value"
+                                            name="Ingresos"
+                                            fill="#60A5FA"
+                                        />
+                                    </BarChart>
+                                </ResponsiveContainer>
+                            </Tab.Panel>
+                            <Tab.Panel>
+                                <ResponsiveContainer width="100%" height={730}>
+                                    <BarChart
+                                        width={1100}
+                                        height={600}
+                                        data={summaries}
+                                        margin={{
+                                            top: 5,
+                                            right: 20,
+                                            left: 0,
+                                            bottom: 5,
+                                        }}
+                                    >
+                                        <CartesianGrid strokeDasharray="3 3" />
+                                        <XAxis dataKey="date" />
+                                        <YAxis />
+                                        <Tooltip />
+                                        <Legend />
+                                        <Bar
+                                            type="monotone"
+                                            dataKey="vehicles"
+                                            name="Vehículos"
+                                            fill="#60A5FA"
+                                        />
+                                    </BarChart>
+                                </ResponsiveContainer>
+                            </Tab.Panel>
+                        </Tab.Panels>
+                    </Tab.Group>
+                </div>
+            )}
+        </>
     )
 }
 
