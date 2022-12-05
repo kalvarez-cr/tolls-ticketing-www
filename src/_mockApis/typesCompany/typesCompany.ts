7d94bc34fa7d9d77b31224237f65dbe1479c83e4
ivana <i.alvarez@rsegrp.com>
2022-12-05 16:48:57 -0400

    Ajustes para cuentas de usuarios

--- src/_mockApis/typesCompany/typesCompany.ts
+++ src/_mockApis/typesCompany/typesCompany.ts
@@ -1,37 +1,38 @@
-export const typesCompany= [
-    // {
-    //     "id": "61804212e554bf6c947be3f1",
-    //     "company_type": "public",
-    //     "name": "Publica",
-    //     "abbreviation": "PUB",
-    //     "description": "Empresa de dominisasao Publico"
-    // },
-    // {
-    //     "id": "61804212e554bf6c947be3f2",
-    //     "company_type": "private",
-    //     "name": "Privada",
-    //     "abbreviation": "PRIV",
-    //     "description": "Empresa de dominio Privado"
-    // },
-    // {
-    //     "id": "61804212e554bf6c947be3f3",
-    //     "company_type": "mix",
-    //     "name": "Mixta",
-    //     "abbreviation": "MIX",
-    //     "description": "Empresa Mixta"
-    // },
-    // {
-    //     "id": "61804212e554bf6c947be3f4",
-    //     "company_type": "asoc_coop",
-    //     "name": "Asociacion Cooperativa",
-    //     "abbreviation": "COOP",
-    //     "description": "Asociacion Cooperativa de Transportistas"
-    // },
-    // {
-    //     "id": "61804212e554bf6c947be3f5",
-    //     "company_type": "owner_operated",
-    //     "name": "Compañia Individual",
-    //     "abbreviation": "ooc",
-    //     "description": "Empresa Operada por su dueño"
-    // }
+export const movements = [
+    {
+        id: '61804212e554bf6c947be3f1',
+        company_type: '01/12',
+        name: '10:30',
+        abbreviation: '',
+        description: '',
+        lane: '',
+        amount: '50.00',
+    },
+    {
+        id: '61804212e554bf6c947be3f2',
+        company_type: '02/12',
+        name: '10:20',
+        abbreviation: 'HAL-217',
+        description: 'Taguanes',
+        lane: '2',
+        amount: '-25.00',
+    },
+    {
+        id: '61804212e554bf6c947be3f3',
+        company_type: '03/12',
+        name: '10:25',
+        abbreviation: 'HAL-217',
+        description: 'Taguanes',
+        lane: '3',
+        amount: '-25.00',
+    },
+    {
+        id: '61804212e554bf6c947be3f3',
+        company_type: '03/12',
+        name: '10:00',
+        abbreviation: '',
+        description: '',
+        lane: '',
+        amount: '50.00',
+    },
 ]
